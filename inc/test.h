0b2121dd99dd815dd9c1e507b9b87ee408c824fe
Giray Yalcin <Giray.Yalcin@synaptics.com>
2018-03-13 18:50:36 -0700

    -adding framework for linux to build.

---
+++ inc/test.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <assert.h>
+
+#include <sched.h>		// To set the priority on linux
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
